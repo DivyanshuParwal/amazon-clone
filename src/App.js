6ab5dfa18a42e0922945b3303d5b615cb4dcdda8
DivyanshuParwal <divyanshuparwal2001@gmail.com>
2020-10-17 15:37:38 +0530

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Route } from 'react-router-dom';
+import React, { Fragment, useContext } from 'react';
+import { Route, Redirect } from 'react-router-dom';
 
 import './App.css';
 import Auth from './Components/Auth/Auth';
@@ -7,10 +7,13 @@ import Logout from './Components/Auth/Logout/Logout';
 import Checkout from './Components/Checkout/Checkout';
 import Home from './Components/Home/Home';
 import NavBar from './Components/NavBar/NavBar';
+import { AuthContext } from './context/authContext';
 
 const App = () => {
-	return (
-		<div className="App">
+	const auth = useContext(AuthContext);
+
+	let links = (
+		<Fragment>
 			<Route path="/" exact>
 				<NavBar />
 				<Home />
@@ -20,9 +23,28 @@ const App = () => {
 				<Checkout />
 			</Route>
 			<Route path="/authenticate" exact component={Auth} />
-			<Route path="/logout" exact component={Logout} />
-		</div>
+			<Redirect to="/" />
+		</Fragment>
 	);
+
+	if (auth.isAuthenticated) {
+		links = (
+			<Fragment>
+				<Route path="/" exact>
+					<NavBar />
+					<Home />
+				</Route>
+				<Route path="/checkout" exact>
+					<NavBar />
+					<Checkout />
+				</Route>
+				<Route path="/logout" exact component={Logout} />
+				<Redirect to="/" />
+			</Fragment>
+		);
+	}
+
+	return <div className="App">{links}</div>;
 };
 
 export default App;
